73189f0bbcfb1de1caae4855497e8376146eeb19
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-02 09:17:48 +0100

    Add project files.

---
+++ Esercizio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Esercizio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filein = "input.txt";
+            string fileout = "output.txt";
+            string numeri;
+            if (File.Exists(filein))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(filein))
+                    {
+                        numeri = reader.ReadLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
