27796010f36f34161be09814f5bee14a79e27871
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-03-02 09:55:10 +0100

    da mettere nel file output

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -6,24 +6,56 @@ namespace Esercizio
     class Program
     {
         static void Main(string[] args)
-        {
-            string filein = "input.txt";
-            string fileout = "output.txt";
-            string numeri;
-            if (File.Exists(filein))
+        {   
+            try
             {
-                try
+                string filein = "input.txt";
+                string fileout = "output.txt";
+                string numeri = " ";
+                string lunghezza = "";
+                int n;
+                int x;
+                int i = 0;
+                char stop;
+                int c;
+                string numero ="";
+
+                if (File.Exists(filein))
                 {
                     using (StreamReader reader = new StreamReader(filein))
                     {
                         numeri = reader.ReadLine();
                     }
                 }
-                catch (Exception ex)
+
+                do
                 {
-                    Console.WriteLine(ex.Message);
+                    stop = numeri[i];
+                    i = i + 1;
+                } while (stop != '1');
+
+                for (c = i; c < i + i - 1; c++)
+                {
+                    lunghezza += int.Parse(numeri[c].ToString());
                 }
+                Console.WriteLine(lunghezza);
+
+                n = int.Parse(lunghezza); 
+
+                for (int j = c; j < c + n; j++)
+                {
+                    numero += numeri[j].ToString();
+                }
+
+                x = int.Parse(numero);
+
+                Console.WriteLine(x);
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
